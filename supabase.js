439806698b99654016876d2b864e797588d37097
belajarkoding-yuk <jalankeong@gmail.com>
2026-09-13 06:16:06 +0700

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://omrhugzyqdpxsmtiidet.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_POu02TEtT7_dPBTnSehz5Q_0Pzj0H6L";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
